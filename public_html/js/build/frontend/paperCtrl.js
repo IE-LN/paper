859ef8a5dd3f2a4d2cb258cc8ad9482e10c17080
zytieren <zytieren@gmail.com>
2014-03-27 17:06:34 -0400

    Angular directive test

--- public_html/js/build/frontend/paperCtrl.js
+++ public_html/js/build/frontend/paperCtrl.js
@@ -1,10 +1,17 @@
 /*papermvc.controller('PaperCtrl', function PaperCtrl($scope) {
 	$scope.data = {message: "Hello Kitties"};
-});*/ 
+});*/
 
 function PaperCtrl($scope){
 	$scope.data = {message: "Hello Kitties"};
 	console.log('test');
 }
+
+papermvc.directive("modalContainer", function () {
+	return {
+		restrict: "E",
+		template: "<div class='page'>Bam! Modal Power!</div>"
+	}
+})
 
 PaperCtrl.$inject = ['$scope'];
