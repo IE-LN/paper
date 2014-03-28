c6fc2a34173c9658b79db074d21b2f1f4a924bae
Willie Fu <willie.fu@gmail.com>
2014-03-28 12:22:15 -0700

    box selector

--- public_html/js/build/frontend/paperCtrl.js
+++ public_html/js/build/frontend/paperCtrl.js
@@ -14,14 +14,16 @@ function PaperCtrl($scope){
 papermvc.directive("modalContainer", function () {
 	return {
 		restrict: "E",
-		template: "<div class='wrapper'><div id='prev' flip-button='prev()'></div><div class='page'><div class='box'></div></div><div id='next' flip-button='next()'></div></div>"
+		template: "<div class='wrapper'><div id='prev' flip-button='prev()'></div><div class='page'><div id='box'></div></div><div id='next' flip-button='next()'></div></div>"
 	}
 })
 
 papermvc.directive("flipButton", function () {
 	return function ( scope, element, attrs ) {
 		element.bind('mousedown', function () {
-			scope.$apply(attrs.flipButton)
+			scope.$apply(attrs.flipButton);
+			
+			console.log(document.getElementById('box'));
 		})
 	}
 })
